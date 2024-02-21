8523467cc9e59e4c36b1c0535759b6f9bfbf8d23
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2024-02-21 19:31:33 +0300

    feat: use print statement(s) as a debugging tool

--- ChasingDebug/ViewController.swift
+++ ChasingDebug/ViewController.swift
@@ -12,6 +12,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("I am inside the viewDidLoad method")
+        
+        ///variadic functions
+        ///separator - provide string that should be placed btw every item in the print() call
+        ///terminator - what should be placed after the end item (default is \n)
+        print(1,2,3,4,5, separator: "-", terminator: "\n")
     }
 
 
